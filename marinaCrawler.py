3d490463e2c04a6c0de18f117180c183638779e7
Kaiku0125 <pianoprince0125@gmail.com>
2021-05-26 22:51:05 +0800

    2021/5/26_crawler worked add progresssbar

--- marinaCrawler.py
+++ marinaCrawler.py
@@ -1,4 +1,4 @@
-
+from __future__ import division
 from selenium import webdriver
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
@@ -8,6 +8,9 @@ import os
 import time
 import requests
 import sys
+import progressbar
+import math
+
 
 from urllib3.packages.six import b
 
@@ -16,11 +19,9 @@ IG_PASSWORD='linkevin'
 
 # 加启动配置
 option = webdriver.ChromeOptions()
-
 option.add_experimental_option('useAutomationExtension', False)
 option.add_experimental_option('excludeSwitches', ['enable-automation'])
-url = 'https://www.instagram.com/coco20002/'
-
+url = 'https://www.instagram.com/coco20002/' #targetUrl
 IGurl = 'https://www.instagram.com'
 chromedriver_path = r"C:\Users\Kaiku\Desktop\crawler\spider\chromedriver.exe"
 
@@ -28,26 +29,93 @@ browser = webdriver.Chrome(executable_path=chromedriver_path,chrome_options=opti
 soup = Soup(browser.page_source, "html.parser")
 media_url = []
 finalall_url = []
-
+SCROLL_PAUSE_TIME = 1
 
 
 def openchrome():
     page = browser.get(url)
 
+def login():
+    WebDriverWait(browser, 30).until(EC.presence_of_element_located((By.NAME, 'username')))
+
+    # ------ 網頁元素定位 ------
+    username_input = browser.find_elements_by_name('username')[0]
+    password_input = browser.find_elements_by_name('password')[0]
+    print("inputing username and password...")
+    # ------ 輸入帳號密碼 ------
+    username_input.send_keys(IG_USERNAME)
+    password_input.send_keys(IG_PASSWORD)
+
+    # ------ 登入 ------
+    WebDriverWait(browser, 30).until(EC.presence_of_element_located((By.XPATH,
+    '//*[@id="loginForm"]/div/div[3]/button/div')))
+    # ------ 網頁元素定位 ------
+    login_click = browser.find_elements_by_xpath('//*[@id="loginForm"]/div/div[3]/button/div')[0]
+    # ------ 點擊登入鍵 ------
+    login_click.click()
+
+    # ------ 不儲存登入資料 ------
+    WebDriverWait(browser, 30).until(EC.presence_of_element_located((By.XPATH, '//*[@id="react-root"]/section/main/div/div/div/div/button')))
+
+    # ------網頁元素定位 ------
+    store_click = browser.find_elements_by_xpath('//*[@id="react-root"]/section/main/div/div/div/div/button')[0]
+
+    # ------ 點擊不儲存鍵 ------
+    store_click.click()
+
+    # ------ 不開啟通知 ------
+    WebDriverWait(browser, 30).until(EC.presence_of_element_located((By.XPATH, '/html/body/div[4]/div/div/div/div[3]/button[2]')))
+
+    # ------ 網頁元素定位 ------                                                                                                    
+    notification_click = browser.find_elements_by_xpath('/html/body/div[4]/div/div/div/div[3]/button[2]')[0]
+
+    # ------ 點擊不開啟通知 ------
+    notification_click.click()
+    print("Log in!")
+
 def main():
     browser.get(url)
-    browser.page_source
-    soup = Soup(browser.page_source, "html.parser")
+    wait = 0
+    print('scrolling...')
+    lenOfPage = browser.execute_script("window.scrollTo(0, document.body.scrollHeight);var lenOfPage=document.body.scrollHeight;return lenOfPage;")
+    scroll = True
+    while scroll == True:
+        lastCount = lenOfPage
+        time.sleep(3)
+        wait +=1
+        lenOfPage = browser.execute_script("window.scrollTo(0, document.body.scrollHeight);var lenOfPage=document.body.scrollHeight;return lenOfPage;")
+        
+        browser.page_source
+        soup = Soup(browser.page_source, "html.parser")
+        # get all browser post url
+        for i in soup.find_all('a', href = True):
+            if i['href'].startswith('/p/'):
+                print('Link found : {0}'.format(i['href']))
+                if i['href'] not in media_url:
+                    media_url.append(IGurl + i['href'])
+        
+        if lastCount==lenOfPage:
+            scroll=False
+        
+        if wait == 20:
+            scroll =False
+
+
+        
+        print('scrolling page :' + str(wait))
+
+        # browser.execute_script("window.scrollTo(0,4000);")
+        # time.sleep(SCROLL_PAUSE_TIME)
+        # wait +=1
+        # print('scrolling page :' + str(wait))
+        # if wait == 40:
+        #     break
+    print('scrolling finish...')
+
     
-    # get all browser post url
-    for i in soup.find_all('a', href = True):
-        if i['href'].startswith('/p/'):
-            print('Link found : {0}'.format(i['href']))
-            media_url.append(IGurl + i['href'])
-    
-    count = 0
-    
+     
     #獲得每個貼文的img
+    count = 0 
     for i in media_url:
         browser.get(i)
         browser.page_source
@@ -55,7 +123,7 @@ def main():
         time.sleep(1)
         # button = browser.find_element_by_class_name('_6CZji')
         try:
-            WebDriverWait(browser, 5).until(EC.presence_of_element_located((By.CLASS_NAME, '_6CZji')))
+            WebDriverWait(browser, 3).until(EC.presence_of_element_located((By.CLASS_NAME, '_6CZji')))
             button = browser.find_elements_by_class_name('_6CZji')[0]
             
         except:
@@ -104,54 +172,25 @@ def main():
         with open("marina\\" + "marinaImg" + str(num) + ".jpg", "wb") as file:  # 開啟資料夾及命名圖片檔
             file.write(image.content)
         num+=1
+        progressbar(num,count)
+        time.sleep(0.25)
 
-    
-
-def login():
-    WebDriverWait(browser, 30).until(EC.presence_of_element_located((By.NAME, 'username')))
-
-    # ------ 網頁元素定位 ------
-    username_input = browser.find_elements_by_name('username')[0]
-    password_input = browser.find_elements_by_name('password')[0]
-    print("inputing username and password...")
-    # ------ 輸入帳號密碼 ------
-    username_input.send_keys(IG_USERNAME)
-    password_input.send_keys(IG_PASSWORD)
-
-    # ------ 登入 ------
-    WebDriverWait(browser, 30).until(EC.presence_of_element_located((By.XPATH,
-    '//*[@id="loginForm"]/div/div[3]/button/div')))
-    # ------ 網頁元素定位 ------
-    login_click = browser.find_elements_by_xpath('//*[@id="loginForm"]/div/div[3]/button/div')[0]
-    # ------ 點擊登入鍵 ------
-    login_click.click()
-
-    # ------ 不儲存登入資料 ------
-    WebDriverWait(browser, 30).until(EC.presence_of_element_located((By.XPATH, '//*[@id="react-root"]/section/main/div/div/div/div/button')))
-
-    # ------網頁元素定位 ------
-    store_click = browser.find_elements_by_xpath('//*[@id="react-root"]/section/main/div/div/div/div/button')[0]
-
-    # ------ 點擊不儲存鍵 ------
-    store_click.click()
-
-    # ------ 不開啟通知 ------
-    WebDriverWait(browser, 30).until(EC.presence_of_element_located((By.XPATH, '/html/body/div[4]/div/div/div/div[3]/button[2]')))
-
-    # ------ 網頁元素定位 ------                                                                                                    
-    notification_click = browser.find_elements_by_xpath('/html/body/div[4]/div/div/div/div[3]/button[2]')[0]
-
-    # ------ 點擊不開啟通知 ------
-    notification_click.click()
-    print("Log in!")
+    # pbar.finish()
+    print( '\n照片已儲存...')
 
 
+def progressbar(cur, total):
+    percent = '{:.2%}'.format(cur / total)
+    sys.stdout.write('\r')
+    sys.stdout.write("[%-50s] %s" % (
+                            '=' * int(math.floor(cur * 50 / total)),
+                            percent))
+    sys.stdout.flush()
 
 if __name__ == '__main__':
     openchrome()
     login()
     main()
-    # print(media_url)
     
 
 
